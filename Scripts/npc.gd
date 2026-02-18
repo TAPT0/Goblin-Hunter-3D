026e42bbaf7109d4239c7c1774a2c5652f5813be
aahil <moominafarash@gmail.com>
2026-02-18 23:29:38 +0530

    made some changes

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,99 +1,88 @@
 extends CharacterBody3D
 
-var player_in_range= false
+var player_in_range = false
 var dialogue_box_node = null
-var text_label_node=null
+var text_label_node = null
 
 func _ready():
-	# check zone exist
+	# Make sure the TalkZone exists
 	if has_node("TalkZone"):
 		$TalkZone.body_entered.connect(_on_body_entered)
 		$TalkZone.body_exited.connect(_on_body_exited)
 
-	$Label3D.visible= false
+	$Label3D.visible = false
 
 func _on_body_entered(body):
 	
 	if body.is_in_group("Player"):
-		player_in_range =true
-		$Label3D.visible = true 
+		player_in_range = true
+		$Label3D.visible=true
 		
-		# find box ui
+		# Try to find the box
 		if body.has_node("HUD/DialogueBox"):
-			dialogue_box_node= body.get_node("HUD/DialogueBox")
+			dialogue_box_node = body.get_node("HUD/DialogueBox")
 			text_label_node = dialogue_box_node.get_node("TextLabel")
 
 func _on_body_exited(body):
 	if body.is_in_group("Player"):
-		player_in_range=false
+		player_in_range = false
 
-		$Label3D.visible =false
+		$Label3D.visible = false
 		if dialogue_box_node:
 			dialogue_box_node.visible = false
-		
-		# mouse capture back
 		Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
 
 @warning_ignore("unused_parameter")
 func _input(event):
-	if Input.is_action_just_pressed("interact") and $Label3D.visible:
+	if Input.is_action_just_pressed("interact")and$Label3D.visible:
 		handle_interaction()
 
 func handle_interaction():
-	var player = get_tree().get_first_node_in_group("Player")
-	
-	# if quest done open shop
-	if dialogue_box_node.visible and player.quest_status == "completed":
+	var player=get_tree().get_first_node_in_group("Player")
+	if dialogue_box_node.visible and player.quest_status=="completed":
 		open_shop()
 		return
 	
-	dialogue_box_node= player.get_node("HUD/DialogueBox")
-	text_label_node= dialogue_box_node.get_node("TextLabel")
+	dialogue_box_node=player.get_node("HUD/DialogueBox")
+	text_label_node=dialogue_box_node.get_node("TextLabel")
 	
-	dialogue_box_node.visible = true
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	dialogue_box_node.visible=true
+	Input.mouse_mode=Input.MOUSE_MODE_VISIBLE
 	
 	if player.quest_status=="none":
 		text_label_node.text="The Goblins are attacking! Kill 5 of them and I will pay you 500 Gold."
 		start_quest(player)
-		
-	elif player.quest_status == "started":
-		# check kill count
-		if player.goblins_killed >= player.quest_goal:
+	elif player.quest_status=="started":
+		if player.goblins_killed>=player.quest_goal:
 			text_label_node.text="You did it! Here is your 500 Gold."
 			finish_quest(player)
 		else:
-			var left= player.quest_goal - player.goblins_killed
-			text_label_node.text="Hurrry! There are still "+str(left)+" goblins left."
-			
-	elif player.quest_status=="completed":
+			var left=player.quest_goal-player.goblins_killed
+			text_label_node.text="Hurrry! There are still"+str(left)+"goblins left."
+	elif player.quest_status =="completed":
 		text_label_node.text="Thank you for saving us.Would you like to buy potions?"
 
 func start_quest(player):
-	player.quest_status ="started"
-	player.quest_active= true
-	player.goblins_killed = 0
-	
-	# show ui label
+	player.quest_status="started"
+	player.quest_active=true
+	player.goblins_killed=0
 	player.get_node("HUD/QuestLabel").visible=true
 	player.get_node("HUD/QuestLabel").text="Mission: Kill Goblins(0/5)"
 	
 func finish_quest(player):
-	player.quest_status="completed"
+	player.quest_status = "completed"
 	player.quest_active = false
 	
-	# give reward
 	if "gold" in player:
-		player.gold+= 500
+		player.gold += 500
 		if player.has_method("update_HUD"):
 			player.update_HUD()
 	
 	if player.has_node("HUD/QuestLabel"):
-		player.get_node("HUD/QuestLabel").visible= false
+		player.get_node("HUD/QuestLabel").visible = false
 
 func open_shop():
-	dialogue_box_node.visible= false
-	var shop= dialogue_box_node.get_parent().get_node("ShopMenu")
-	
-	shop.visible= true
-	Input.mouse_mode= Input.MOUSE_MODE_VISIBLE
+	dialogue_box_node.visible=false
+	var shop=dialogue_box_node.get_parent().get_node("ShopMenu")
+	shop.visible=true
+	Input.mouse_mode=Input.MOUSE_MODE_VISIBLE
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,112 +1,100 @@
 extends CharacterBody3D
 
-var quest_status= "none"
+var quest_status = "none"
 var quest_active = false
-var goblins_killed= 0
-var quest_goal= 5
-
-# speed/jump
-var SPEED= 5.0
-const JUMP_VELOCITY= 4.5
+var goblins_killed = 0
+var quest_goal = 5
+#speed/jump
+var SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 var is_ground_slamming = false
 
-var is_blocking= false
-var last_block_time= 0.0
+var is_blocking = false
+var last_block_time = 0.0
 
-var is_attacking= false
-@onready var anim_playback= $MainAnimator
+var is_attacking = false
+@onready var anim_playback = $MainAnimator
 
-@export var shockwave_scene:PackedScene
+@export var shockwave_scene: PackedScene
 
 @export var slam_damage = 50
 @export var slam_speed = 25.0
-
-# dash
-@export var dash_speed:float= 20.0
+#dash
+@export var dash_speed: float = 20.0
 @export var damage_indicator_scene: PackedScene
-var is_dashing:bool = false
-var gravity= ProjectSettings.get_setting("physics/3d/default_gravity")
+var is_dashing: bool = false
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var sensivity= 0.003
-var onCooldown= false
+var sensivity = 0.003
 
-# gold,hp,damage and stamina 
+
+var onCooldown = false
+
+#gold,hp,damage and stamina 
 var stamina = 100.0
-var max_stamina= 100.0
+var max_stamina = 100.0
 var stamina_regen_rate = 20.0
 var dash_cost = 30.0
-var gold= 0
+var gold = 0
 var hp = 50
-var maxhp= 50
-var damage= 10
-var target= []
+var maxhp = 50
+var damage = 10
+var target = []
 
 
 @onready var quest_label = $HUD/QuestLabel
-@onready var game_over_screen= $HUD/GameOverScreen
-
-# stamina bar /hp bar and stuff
+@onready var game_over_screen = $HUD/GameOverScreen
+#stamina bar /hp bar and stuff
 @onready var staminabar = $HUD/StaminaBar
-@onready var hpbar= $HUD/HpBar
+@onready var hpbar = $HUD/HpBar
 @onready var goldlabel = $HUD/GoldLabel
-@onready var camera= $Head/FirstPerson
-@onready var animationplayer= $MainAnimator
+@onready var camera = $Head/FirstPerson
+@onready var animationplayer = $MainAnimator
 @onready var cooldown = $AttackCooldown
-@onready var anim_movement= $character/AnimationPlayer
+@onready var anim_movement = $character/AnimationPlayer
 @onready var anim_combat = $MainAnimator
 
 func player():
 	pass
 
 func _ready():
-	is_blocking= false
+	is_blocking = false
 	$MainAnimator.stop()
-	
 	if has_node("character/AnimationPlayer"):
 		$character/AnimationPlayer.play("mixamo_com")
-		
 	anim_movement.play("mixamo_com") 
 	anim_combat.stop()
 	add_to_group("Player")
-	
-	$Head/FirstPerson.current= true
-	Input.mouse_mode= Input.MOUSE_MODE_CAPTURED
-	
+	$Head/FirstPerson.current = true
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	await get_tree().create_timer(0.1).timeout
 	load_game()
 	update_HUD()
 
 func attack():
-	hpbar.max_value= 50
-	# check can attack
+	hpbar.max_value = 50
 	if Input.is_action_just_pressed("attack") and not onCooldown and not is_blocking:
-		
 		is_attacking = true
-		onCooldown= true
+		onCooldown = true
 		$AttackCooldown.start()
-		
-		anim_movement.stop()       
+		anim_movement.stop()      
 		anim_combat.play("SwordSlash")  
 		if has_node("SwordSound"): $SwordSound.play()
-		
 		deal_damage()
-		
 		await get_tree().create_timer(0.8).timeout
-		is_attacking= false
+		is_attacking = false
 
 func deal_damage():
 	if target.size() > 0:
 		GlobalEffects.hit_stop()
 		#$Head/FirstPerson.apply_shake(0.01)
-		
 		for enemies in target:
 			enemies.hp -= damage
 			
 			if enemies.has_method("apply_knockback"):
-				var push_direction= (enemies.global_position - global_position).normalized()
+				var push_direction = (enemies.global_position - global_position).normalized()
 				push_direction.y = 0
 				enemies.apply_knockback(push_direction, 4.0)
-
 #func _switch_view():
 	#if Input.is_action_just_pressed("switch"):
 	#
@@ -118,70 +106,58 @@ func deal_damage():
 			#$Head/FirstPerson.current = true
 			##$Head/ThirdPerson.current=true
 			#
-
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * sensivity)
 		$Head.rotate_x(-event.relative.y * sensivity)
-		$Head.rotation.x= clamp($Head.rotation.x, deg_to_rad(-60), deg_to_rad(70))
+		$Head.rotation.x = clamp($Head.rotation.x, deg_to_rad(-60), deg_to_rad(70))
 
 @warning_ignore("unused_parameter")
 func _process(delta):
 	update_HUD()
 	attack()
 	#_switch_view()
-	
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
-		
-		# reset pos bug fix attempt
-		var all_nodes= find_children("*", "Node3D", false)
+		var all_nodes = find_children("*", "Node3D", false)
 		for node in all_nodes:
 			if node.name == "character" or node.name == "Visuals" or node.name == "Node":
-				node.position= Vector3(0, 0, 0)
+				node.position = Vector3(0, 0, 0)
 				
 				if node.has_node("Node"):
-					node.get_node("Node").position= Vector3(0, 0, 0)
+					node.get_node("Node").position = Vector3(0, 0, 0)
 
 func _physics_process(delta):
-	# gravity logic
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y= JUMP_VELOCITY
-		
+		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_pressed("dash") and not is_dashing and stamina >= dash_cost:
 		start_dash()
-		stamina-= dash_cost
-		staminabar.value= stamina
-		
-	var current_speed= SPEED
+		stamina -= dash_cost
+		staminabar.value = stamina
+	var current_speed = SPEED
 	if is_dashing:
-		current_speed= dash_speed
+		current_speed = dash_speed
 		
-	var input_dir= Input.get_vector("left","right","up","down")
-	var direction= (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if Input.is_action_pressed("block"):
-		is_blocking= true
-		velocity.x= 0
-		velocity.z= 0
+		is_blocking = true
+		velocity.x = 0
+		velocity.z = 0
 	else:
 		is_blocking = false
-		
 	if not is_blocking:
 		if direction:
-			velocity.x= direction.x * current_speed
-			velocity.z= direction.z * current_speed
+			velocity.x = direction.x * current_speed
+			velocity.z = direction.z * current_speed
 		else:
-			velocity.x= move_toward(velocity.x, 0, current_speed)
-			velocity.z= move_toward(velocity.z, 0, current_speed)
-			
+			velocity.x = move_toward(velocity.x, 0, current_speed)
+			velocity.z = move_toward(velocity.z, 0, current_speed)
 	if is_blocking:
 		anim_combat.play("BlockIdle")
 		if has_node("character/AnimationPlayer"): $character/AnimationPlayer.stop()
-		
 	elif is_attacking:
 		pass
 	else:
@@ -189,21 +165,19 @@ func _physics_process(delta):
 			anim_combat.play("RESET")
 		else:
 			anim_combat.stop()
-			
 		if velocity.length() > 0:
 			if has_node("character/AnimationPlayer"): $character/AnimationPlayer.play("RunForward")
 		else:
 			if has_node("character/AnimationPlayer"): $character/AnimationPlayer.play("mixamo_com")
-			
 	if not is_on_floor() and Input.is_action_just_pressed("attack") and not is_ground_slamming:
 		start_ground_slam()
 		
 	if is_ground_slamming:
-		velocity.x= 0
-		velocity.z= 0
-		velocity.y= -slam_speed
+		velocity.x = 0
+		velocity.z = 0
+		velocity.y = -slam_speed
 		
-		if is_on_floor():   
+		if is_on_floor():    
 			finish_ground_slam()
 			
 	move_and_slide()
@@ -217,20 +191,19 @@ func _physics_process(delta):
 		else:
 			if anim_movement.current_animation != "mixamo_com": 
 				anim_movement.play("mixamo_com")
-				
-	# regenerate stamina
+	#regenerate stamina
 	if not is_dashing and stamina < max_stamina:
-		stamina+= stamina_regen_rate * delta
+		stamina += stamina_regen_rate * delta
 		if stamina > max_stamina:
-			stamina= max_stamina
-			staminabar.value= stamina
+			stamina = max_stamina
+			staminabar.value = stamina
 
 func _on_attack_cooldown_timeout():
-	onCooldown= false
+	onCooldown = false
 
 func update_HUD():
-	hpbar.value= hp
-	goldlabel.text= "Gold:"+str(gold)
+	hpbar.value = hp
+	goldlabel.text = "Gold:" + str(gold)
 
 
 func _on_attack_zone_body_entered(body):
@@ -243,41 +216,39 @@ func _on_attack_zone_body_exited(body):
 		target.erase(body)
 
 func start_dash():
-	is_dashing= true
+	is_dashing = true
 	$DashTimer.start()
 
 func _on_dash_timer_timeout():
 	is_dashing = false
 
 func take_damage(amount):
-	
 	if is_blocking:
-		var time_since_block= Time.get_ticks_msec() - last_block_time
+		var time_since_block = Time.get_ticks_msec() - last_block_time
 		if time_since_block < 250:
-			return # perfect block
+			return
 		else:
-			amount= amount/2
-			
-	hp-= amount
+			amount = amount / 2
+	hp -= amount
 	update_HUD()
 	if hp <= 0:
 		die()
 		
 func die():
-	get_tree().paused= true
+	get_tree().paused = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	game_over_screen.visible= true
+	game_over_screen.visible = true
 	
 func add_kill():
 	if quest_active:
-		goblins_killed+= 1
-		quest_label.text= "Mission:Kill Goblins("+str(goblins_killed)+"/"+str(quest_goal)+")"
+		goblins_killed += 1
+		quest_label.text = "Mission:Kill Goblins(" + str(goblins_killed) + "/" + str(quest_goal) + ")"
 		if goblins_killed >= quest_goal:
-			quest_label.text= "Mission Complete! Return to Mage."
+			quest_label.text = "Mission Complete! Return to Mage."
 			quest_label.modulate = Color.GREEN
 
 func save_game():
-	var save_data={
+	var save_data = {
 		"hp": hp,
 		"max_hp": maxhp,
 		"stamina": stamina,
@@ -293,10 +264,9 @@ func save_game():
 		"rotation_Y": rotation.y,
 		"scene_path": get_tree().current_scene.scene_file_path
 	}
-	
-	var file= FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
 	if file:
-		var json_string= JSON.stringify(save_data)
+		var json_string = JSON.stringify(save_data)
 		file.store_line(json_string)
 		file.close()
 		show_save_notification()
@@ -306,14 +276,13 @@ func load_game():
 	if not FileAccess.file_exists("user://savegame.save"):
 		return
 		
-	var file= FileAccess.open("user://savegame.save", FileAccess.READ)
+	var file = FileAccess.open("user://savegame.save", FileAccess.READ)
 	if not file:
 		return false
-		
-	var json_string= file.get_line()
+	var json_string = file.get_line()
 	file.close()
 	
-	var data= JSON.parse_string(json_string)
+	var data = JSON.parse_string(json_string)
 	
 	if data:
 		hp = data.get("hp", 50)
@@ -326,23 +295,23 @@ func load_game():
 		quest_active = data.get("quest_active", false)
 		
 		if data.has("position_x"):
-			global_position= Vector3(
+			global_position = Vector3(
 				data["position_x"],
 				data["position_y"],
 				data["position_z"]
 			)
 			if data.has("rotation_y"):
-				rotation.y= data["rotation_y"]
+				rotation.y = data["rotation_y"]
 				
 				update_HUD()
 				return true
 
 func show_save_notification():
-	var label= Label.new()
-	label.text= "Game Saved!"
+	var label = Label.new()
+	label.text = "Game Saved!"
 	label.add_theme_font_size_override("font_size", 32)
-	label.modulate= Color.GREEN
-	label.position= Vector2(get_viewport().size.x/2 - 100, 100)
+	label.modulate = Color.GREEN
+	label.position = Vector2(get_viewport().size.x / 2 - 100, 100)
 	
 	$HUD.add_child(label)
 	
@@ -360,29 +329,29 @@ func get_quest_data():
 @warning_ignore("unused_parameter")
 func _input(event):
 	if Input.is_action_just_pressed("torch"):
-		$character/Node/Skeleton3D/LeftHandAttachment/Torch.visible= not $character/Node/Skeleton3D/LeftHandAttachment/Torch.visible
+		$character/Node/Skeleton3D/LeftHandAttachment/Torch.visible = not $character/Node/Skeleton3D/LeftHandAttachment/Torch.visible
 
 func start_ground_slam():
-	is_ground_slamming= true
-	velocity.y= 0
+	is_ground_slamming = true
+	velocity.y = 0
 	await get_tree().create_timer(0.2).timeout
 	
 func finish_ground_slam():
-	is_ground_slamming= false
+	is_ground_slamming = false
 	
 	if shockwave_scene:
-		var effect= shockwave_scene.instantiate()
+		var effect = shockwave_scene.instantiate()
 		get_parent().add_child(effect)
 		effect.global_position = global_position
 	
-	var impact_area= Area3D.new()
-	var collision= CollisionShape3D.new()
-	var sphere= SphereShape3D.new()
-	sphere.radius= 3.0
+	var impact_area = Area3D.new()
+	var collision = CollisionShape3D.new()
+	var sphere = SphereShape3D.new()
+	sphere.radius = 3.0
 	
 	add_child(impact_area)
 	impact_area.add_child(collision)
-	collision.shape= sphere
+	collision.shape = sphere
 	
 	await get_tree().process_frame
 	
@@ -403,10 +372,10 @@ func _on_sword_hitbox_body_entered(body):
 				body.take_damage(50)
 				
 				if damage_indicator_scene:
-					var indicator= damage_indicator_scene.instantiate()
+					var indicator = damage_indicator_scene.instantiate()
 					get_tree().current_scene.add_child(indicator)
 					
-					indicator.global_position= body.global_position+ Vector3(0, 1.5, 0)
+					indicator.global_position = body.global_position + Vector3(0, 1.5, 0)
 					
 					if indicator.has_method("set-damage"):
 						indicator.set_damage(50)
